6e7358afe25d989f22ea5497dc84b29e749baa84
Caner Demirci <caner.demirci@outlook.com>
2020-06-18 23:54:18 +0300

    Create Thread_Safe_Singleton.cs

---
+++ Thread_Safe_Singleton.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace OOP
+{
+    class Thread_Safe_Singleton
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();       
+            
+            customerManager.Save();     
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        static object _lockObject = new object();
+
+        private CustomerManager() {}
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                    _customerManager = new CustomerManager();
+            }
+
+            return _customerManager;
+        }
+
+        public void Save() => Console.WriteLine("Saved");
+    }
+}
